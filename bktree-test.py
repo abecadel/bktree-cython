5cf920a7c0b26f68df6c333474da7006121ca901
michal jamry <jamrymi@gmail.com>
2018-05-06 04:54:12 +0200

    tests

--- bktree-test.py
+++ bktree-test.py
@@ -22,5 +22,30 @@ def test_word(word, radius):
 
 
 if __name__ == "__main__":
-    for w, r in [('cat', 1), ('paper', 1), ('elephant', 1), ('Africa', 1), ('cat', 2), ('paper', 2), ('elephant', 2), ('Africa', 2)]:
+    for w, r in [
+        ('cat', 1),
+        ('paper', 1),
+        ('elephant', 1),
+        ('Africa', 1),
+        ('cat', 2),
+        ('paper', 2),
+        ('elephant', 2),
+        ('Africa', 2),
+        ('adam', 1),
+        ('ethos', 1),
+        ('demonstration', 1),
+        ('Terrance', 1),
+        ('revery', 1),
+        ('darnedest', 1),
+        ('legislating', 1),
+        ('Rolando', 1),
+        ('balalaika\'s', 2),
+        ('swoop\'s', 2),
+        ('bobsledding', 1),
+        ('tribulations', 1),
+        ('blunderer', 1),
+        ('scholarship', 1),
+        ('editors', 1),
+        ('unendurable', 1)
+    ]:
         test_word(w, r)
--- bktree.pyx
+++ bktree.pyx
@@ -2,6 +2,21 @@ from collections import deque
 
 def hamming_distance(l, r):
     """
+    >>> zero = 'paper'
+    >>> ones = ['paaer', 'paperr', 'pape']
+    >>> twos = ['peeer', 'pap']
+    >>> hamming_distance(zero, 'paper')
+    0
+    >>> hamming_distance(ones[0], 'paper')
+    1
+    >>> hamming_distance(ones[1], 'paper')
+    1
+    >>> hamming_distance(ones[2], 'paper')
+    1
+    >>> hamming_distance(twos[0], 'paper')
+    2
+    >>> hamming_distance(twos[1], 'paper')
+    2
 
     :param l:
     :param r:
@@ -31,7 +46,7 @@ class BKTree:
 
     """
 
-    def __init__(self, distance_func=hamming_distance):
+    def __init__(self, distance_func=hamming_distance, words=None):
         """
 
         :param distance_func:
@@ -39,19 +54,24 @@ class BKTree:
         self._tree = None
         self._distance_func = distance_func
 
-    def add(self, node):
+        if words is not None:
+            self.add(words)
+
+    def add(self, words):
         """
 
         :param node:
         :return:
         """
-        if type(node) is list:
-            for word in node:
+        if type(words) is list:
+            for word in words:
                 self.__add(word)
         else:
-            self.__add(node)
+            self.__add(words)
 
     def __add(self, node):
+        node = node.strip().lower()
+
         if self._tree is None:
             self._tree = (node, {})
             return
@@ -84,6 +104,10 @@ class BKTree:
                 result.append((dist, candidate))
 
             low, high = dist - radius, dist + radius
-            candidates.extend(c for d, c in children.items()
-                              if low <= d <= high)
+            candidates.extend(c for d, c in children.items() if low <= d <= high)
         return result
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
